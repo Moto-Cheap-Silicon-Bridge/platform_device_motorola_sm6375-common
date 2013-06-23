78179a3e3fc334af489d9e273d111b1927ed7559
blunden <blunden2@gmail.com>
2013-06-23 09:20:46 +0200

    sm6375-common: Declare support for Mifare tags
    d855 variant uses the NXP PN547/PN65T chip
    and supports Mifare tags. This adds the xml file
    to make NXP's apps work properly.
    
    MifareClassicTool further confirms such support as stated in
    https://github.com/ikarus23/MifareClassicTool/blob/master/COMPATIBLE_DEVICES.md.
    
    Change-Id: I9f2f04d3c5c211185bf2ab0d5a797070c49d4b98

--- common.mk
+++ common.mk
@@ -309,6 +309,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
