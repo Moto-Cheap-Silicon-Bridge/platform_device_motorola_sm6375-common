dc0de9ac2ad8267eb03aa5d78eccd2e42000c5ee
kubersharma001 <kubersharma001@gmail.com>
2021-08-27 17:25:10 +0000

    sm6375-common: Import powerhint.json from coral
    * From: https://android.googlesource.com/device/google/coral/+/android-11.0.0_r42/powerhint.json
    
    Change-Id: I8dba2912afb27e0432bdce601ce38f2934bb0186

--- common.mk
+++ common.mk
@@ -328,6 +328,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
