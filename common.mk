e52efe277c798088cab2b6bdb28c408ed09a1120
Bruno Martins <bgcngm@gmail.com>
2023-11-13 13:48:00 +0000

    sm6375-common: Move to QTI health AIDL service
    The health AIDL HAL service provides functionalities of charger

--- common.mk
+++ common.mk
@@ -190,9 +190,9 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery \
+    android.hardware.health@2.1.vendor
 
 # HIDL
 PRODUCT_PACKAGES += \
