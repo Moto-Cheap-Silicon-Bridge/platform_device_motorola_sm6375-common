8022cec53aac939f03be7f22c4280c7c7c597428
Michael Bestas <mkbestas@lineageos.org>
2024-07-01 07:20:40 +0200

    sm6375-common: Switch to common QTI vibrator HAL
    Change-Id: I2974138ee0b86f17f4940dd4d6ff6c690a31c101

--- common.mk
+++ common.mk
@@ -411,8 +411,7 @@ PRODUCT_PACKAGES += \
     vndservicemanager
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
