31d988909908c9bc80184230a73fb3e050a15cde
ViShal69x <tk5762996@gmail.com>
2024-10-05 21:48:49 +0530

    sm6375-common: Set Kernel Source Dir overridable

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,7 +49,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc LLVM=1
-TARGET_KERNEL_SOURCE := kernel/motorola/msm-5.4
+TARGET_KERNEL_SOURCE ?= kernel/motorola/msm-5.4
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
