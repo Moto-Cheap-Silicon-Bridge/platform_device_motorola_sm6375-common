fd77ec648f200bd709e4b2c72068d4f973f69193
lahaina <nattolecats@gmail.com>
2024-11-06 18:32:55 +0900

    tmp

---
+++ extract-files.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixup_remove_arch_suffix,
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_clang_rt_ubsan,
+    libs_proto_3_9_1,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+
+namespace_imports = [
+    'device/motorola/sm6375-common',
+    "hardware/qcom-caf/sm8350",
+    "hardware/qcom-caf/wlan",
+    "vendor/qcom/opensource/commonsys-intf/display",
+    "vendor/qcom/opensource/commonsys/display",
+    "vendor/qcom/opensource/dataservices",
+    "vendor/qcom/opensource/display",
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_vendor' if partition in ['odm', 'vendor'] else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
+        'vendor.qti.diaghal@1.0',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libOmxCore',
+        'libwpa_client',
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .remove_needed('android.hidl.base@1.0.so'),
+    'system_ext/etc/permissions/moto-telephony.xml': blob_fixup()
+        .regex_replace('system', 'system_ext'),
+
+
+
+
+    'odm/bin/hw/vendor.pixelworks.hardware.display.iris-service': blob_fixup()
+        .add_needed('libprocessgroup.so'),
+    ('odm/lib64/mediadrm/libwvdrmengine.so', 'odm/lib64/libwvhidl.so'): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+    'odm/lib64/libAlgoProcess.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V1-ndk_platform.so', 'android.hardware.graphics.common-V5-ndk.so'),
+    'product/app/PowerOffAlarm/PowerOffAlarm.apk': blob_fixup()
+        .apktool_patch('blob-patches/PowerOffAlarm.patch', '-s'),
+    'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()
+        .regex_replace('/my_product', '/product'),
+    'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+    'system_ext/lib/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V3-cpp.so'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so')
+        .add_needed('libinput_shim.so'),
+    ('vendor/etc/media_lahaina/video_system_specs.json', 'vendor/etc/media_yupik_v1/video_system_specs.json'): blob_fixup()
+        .regex_replace('"max_retry_alloc_output_timeout": 1000,', '"max_retry_alloc_output_timeout": 0,'),
+    ('vendor/etc/media_codecs_lahaina.xml', 'vendor/etc/media_codecs_lahaina_vendor.xml', 'vendor/etc/media_codecs_yupik_v1.xml'): blob_fixup()
+        .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).*\n', ''),
+    'vendor/etc/msm_irqbalance.conf': blob_fixup()
+        .regex_replace('IGNORED_IRQ=27,23,38$', 'IGNORED_IRQ=27,23,38,115,332'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'sm8350-common',
+    'oneplus',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
